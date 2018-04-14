768e5fee59ca60b15ed2ab1efc36340db2f1d4e4
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-14 09:19:24 +0200

    added jBCrypt as dependency, implemented BCrypt encryption schema

--- src/main/java/socialgossip/server/security/BcryptSchema.java
+++ src/main/java/socialgossip/server/security/BcryptSchema.java
@@ -1,36 +1,61 @@
 package socialgossip.server.security;
 
+import org.mindrot.jbcrypt.BCrypt;
 import socialgossip.server.core.entities.password.EncryptedPassword;
 import socialgossip.server.core.entities.password.EncryptedSchema;
 
-public class BcryptSchema implements EncryptedSchema<byte[]> {
+import java.util.Objects;
+
+/**
+ * {@link EncryptedSchema} implementation for the {@link BCrypt} encryption algorithm.
+ */
+public class BcryptSchema implements EncryptedSchema<String> {
 
     @Override
-    public EncryptedPassword<byte[]> from(String password) {
-        throw new RuntimeException("implement me!");
+    public EncryptedPassword<String> from(final String password) {
+        return encryptFrom(password);
     }
 
-    public final static class BcryptPassword extends EncryptedPassword<byte[]> {
+    /**
+     * Returns a new encrypted {@link BcryptPassword} from a plain-text password.
+     * @param password is the plain-text password.
+     * @throws NullPointerException if the input password is null.
+     * @return a new encrypted {@link BcryptPassword} password.
+     */
+    public BcryptPassword encryptFrom(final String password) {
+        final String notNullPassword = Objects.requireNonNull(password);
+        final String salt = BCrypt.gensalt();
+        final String hash = BCrypt.hashpw(notNullPassword, salt);
+        return new BcryptPassword(hash, salt);
+    }
 
-        BcryptPassword(byte[] hash, byte[] salt) {
+    /**
+     * Encrypted {@link socialgossip.server.core.entities.password.Password}
+     * using the {@link BCrypt} encryption schema.
+     */
+    public final static class BcryptPassword extends EncryptedPassword<String> {
+
+        private BcryptPassword(String hash, String salt) {
             super(hash, salt);
         }
 
         /**
          * Creates a new {@link BcryptPassword} from hash and salt parameters.
-         * Executes a prevalidation to ensure it conforms to Bcrypt algorithm.
+         * Executes a pre-validation to ensure it conforms to BCrypt algorithm.
          *
          * @param hash is the encrypted password hash.
          * @param salt is the encrypted password salt.
          * @return a {@link BcryptPassword} instance with hash and salt.
          */
-        public static BcryptPassword from(byte[] hash, byte[] salt) {
-            throw new RuntimeException("implement me!");
+        public static BcryptPassword from(String hash, String salt) {
+            // TODO(ar3s3ru): add a validation step.
+            return new BcryptPassword(hash, salt);
         }
 
         @Override
-        public boolean verify(String password) {
-            return false;
+        public boolean verify(final String password) {
+            final String notNullPassword = Objects.requireNonNull(password);
+            return BCrypt.checkpw(notNullPassword, this.getHash());
         }
     }
 }
