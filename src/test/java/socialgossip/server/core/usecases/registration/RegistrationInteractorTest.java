cfb8949a4a803d6b02e73a95dc9b8b064ee2982f
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-20 11:28:09 +0200

    added testing on RegistrationInteractorTest

--- src/test/java/socialgossip/server/core/usecases/registration/RegistrationInteractorTest.java
+++ src/test/java/socialgossip/server/core/usecases/registration/RegistrationInteractorTest.java
@@ -1,11 +1,23 @@
 package socialgossip.server.core.usecases.registration;
 
+import org.junit.Assert;
 import org.junit.Test;
 import socialgossip.server.core.entities.password.EncryptionSchema;
+import socialgossip.server.core.entities.password.InvalidPasswordException;
+import socialgossip.server.core.entities.password.PasswordValidator;
+import socialgossip.server.core.entities.user.InvalidUserException;
 import socialgossip.server.core.entities.user.User;
+import socialgossip.server.core.gateways.GatewayException;
 import socialgossip.server.core.gateways.user.AddUserAccess;
+import socialgossip.server.core.gateways.user.UserAlreadyExistsException;
+import socialgossip.server.security.BcryptSchema;
+import socialgossip.server.security.SimplePasswordValidator;
+
+import java.util.IllformedLocaleException;
+import java.util.Locale;
 
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class RegistrationInteractorTest {
     @Test(expected = NullPointerException.class)
@@ -29,5 +41,83 @@ public class RegistrationInteractorTest {
         final AddUserAccess userGateway = mock(AddUserAccess.class);
         final EncryptionSchema encryptionSchema = mock(EncryptionSchema.class);
         new RegistrationInteractor(User::new, userGateway, encryptionSchema, null);
+    }
+
+    @Test
+    public void usingMockedConstructionArguments() {
+        final UserFactory userFactory = mock(UserFactory.class);
+        final AddUserAccess userAccess = mock(AddUserAccess.class);
+        final EncryptionSchema encryptionSchema = mock(EncryptionSchema.class);
+        final LocaleBuilderFactory localeBuilderFactory = mock(LocaleBuilderFactory.class);
+        new RegistrationInteractor(userFactory, userAccess, encryptionSchema, localeBuilderFactory);
+    }
+
+    @Test
+    public void runWithoutErrors() {
+        final PasswordValidator passwordValidator = new SimplePasswordValidator();
+        final EncryptionSchema<String> encryptionSchema = new BcryptSchema(passwordValidator);
+
+        final AddUserAccess userAccess = mock(AddUserAccess.class);
+        try {
+            doReturn(true).when(userAccess).add(any(User.class));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        final RegistrationInteractor useCase = new RegistrationInteractor(
+                User::new, userAccess, encryptionSchema
+        );
+
+        useCase.execute(new RegistrationUseCase.Input() {
+            @Override
+            public String getUsername() {
+                return "hello";
+            }
+
+            @Override
+            public String getPassword() {
+                return "helloworld";
+            }
+
+            @Override
+            public String getLanguage() {
+                return Locale.getDefault().getISO3Language();
+            }
+
+            @Override
+            public String getRequestId() {
+                return "test";
+            }
+        }, Assert::assertTrue, new RegistrationErrors() {
+            @Override
+            public void onInvalidLanguage(IllformedLocaleException e) {
+                fail(e.getMessage());
+            }
+
+            @Override
+            public void onInvalidUser(InvalidUserException e) {
+                fail(e.getMessage());
+            }
+
+            @Override
+            public void onUserAlreadyExists(UserAlreadyExistsException e) {
+                fail(e.getMessage());
+            }
+
+            @Override
+            public void onGatewayError(GatewayException e) {
+                fail(e.getMessage());
+            }
+
+            @Override
+            public void onInvalidPassword(InvalidPasswordException e) {
+                fail(e.getMessage());
+            }
+
+            @Override
+            public void onError(Exception e) {
+                fail(e.getMessage());
+            }
+        });
     }
 }
