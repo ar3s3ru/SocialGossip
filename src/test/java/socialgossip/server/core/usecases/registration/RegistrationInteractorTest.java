b6674ecbbdb777c3137d2a72a58f4f2223bd14fd
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-20 09:57:07 +0200

    added Registration testing, still WIP

--- src/test/java/socialgossip/server/core/usecases/registration/RegistrationInteractorTest.java
+++ src/test/java/socialgossip/server/core/usecases/registration/RegistrationInteractorTest.java
@@ -1,10 +1,33 @@
 package socialgossip.server.core.usecases.registration;
 
 import org.junit.Test;
+import socialgossip.server.core.entities.password.EncryptionSchema;
+import socialgossip.server.core.entities.user.User;
+import socialgossip.server.core.gateways.user.AddUserAccess;
+
+import static org.mockito.Mockito.*;
 
 public class RegistrationInteractorTest {
-    @Test
-    public void testFn() {
+    @Test(expected = NullPointerException.class)
+    public void usingNullUserFactory() {
+        new RegistrationInteractor(null, null, null, null);
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void usingNullUserGateway() {
+        new RegistrationInteractor(User::new, null, null, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void usingNullEncryptionSchema() {
+        final AddUserAccess userGateway = mock(AddUserAccess.class);
+        new RegistrationInteractor(User::new, userGateway, null, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void usingNullLocaleBuilder() {
+        final AddUserAccess userGateway = mock(AddUserAccess.class);
+        final EncryptionSchema encryptionSchema = mock(EncryptionSchema.class);
+        new RegistrationInteractor(User::new, userGateway, encryptionSchema, null);
     }
 }
