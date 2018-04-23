21e3607464f979856174cce924132b343ec7b3e4
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-23 19:23:01 +0200

    documented SessionScope

--- src/main/java/socialgossip/server/core/entities/session/SessionScoped.java
+++ src/main/java/socialgossip/server/core/entities/session/SessionScoped.java
@@ -1,5 +1,12 @@
 package socialgossip.server.core.entities.session;
 
+/**
+ * Represents an object that has a scope tied to the {@link Session} that carries along.
+ *
+ * An example would be a {@link socialgossip.server.core.gateways.notifications.Notification},
+ * in which case the {@link SessionScoped} object represents the {@link Session} that
+ * generated the {@link socialgossip.server.core.gateways.notifications.Notification} itself.
+ */
 public interface SessionScoped {
     Session from();
 }
