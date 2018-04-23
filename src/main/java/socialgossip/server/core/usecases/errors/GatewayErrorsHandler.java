c6124984b79c223e01f086f720e7c5aef0096289
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-23 22:12:29 +0200

    documented usecases/errors package

--- src/main/java/socialgossip/server/core/usecases/errors/GatewayErrorsHandler.java
+++ src/main/java/socialgossip/server/core/usecases/errors/GatewayErrorsHandler.java
@@ -3,6 +3,9 @@ package socialgossip.server.core.usecases.errors;
 import socialgossip.server.core.gateways.GatewayException;
 import socialgossip.server.core.usecases.ErrorsHandler;
 
+/**
+ * Error handler to be used when accessing a gateway that can cause error.
+ */
 public interface GatewayErrorsHandler extends ErrorsHandler {
     void onGatewayError(GatewayException e);
 }
--- src/main/java/socialgossip/server/core/usecases/errors/PasswordErrorsHandler.java
+++ src/main/java/socialgossip/server/core/usecases/errors/PasswordErrorsHandler.java
@@ -3,6 +3,9 @@ package socialgossip.server.core.usecases.errors;
 import socialgossip.server.core.entities.password.InvalidPasswordException;
 import socialgossip.server.core.usecases.ErrorsHandler;
 
+/**
+ * Error handler to be used when passwords have to be provided and verified.
+ */
 public interface PasswordErrorsHandler extends ErrorsHandler {
     void onInvalidPassword(InvalidPasswordException e);
 }
