500aa9d927da2173d84020fb51d52f188aaadc81
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-23 22:10:25 +0200

    documented gateways/user package

--- src/main/java/socialgossip/server/core/gateways/user/GetUserAccess.java
+++ src/main/java/socialgossip/server/core/gateways/user/GetUserAccess.java
@@ -3,6 +3,17 @@ package socialgossip.server.core.gateways.user;
 import socialgossip.server.core.entities.user.User;
 import socialgossip.server.core.gateways.GatewayException;
 
+/**
+ * Represents a DAO that gives ability to retrieve {@link User} objects by
+ * a number of paramenters (username, etc.)
+ */
 public interface GetUserAccess {
+    /**
+     * Gets an {@link User} by its {@code username}, if exists.
+     * @param username is the {@link User} name to retrieve.
+     * @return an {@link User} that has the specified {@code username}.
+     * @throws UserNotFoundException if no {@link User} with that {@code username} has been found.
+     * @throws GatewayException if an external error with the persistence layer occurred.
+     */
     User getByUsername(String username) throws UserNotFoundException, GatewayException;
 }
--- src/main/java/socialgossip/server/core/gateways/user/UserNotFoundException.java
+++ src/main/java/socialgossip/server/core/gateways/user/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package socialgossip.server.core.gateways.user;
 
+/**
+ * Exception raised when an {@link socialgossip.server.core.entities.user.User}
+ * with a specified {@code username} has not been found.
+ */
 public class UserNotFoundException extends Exception {
     public UserNotFoundException(final String username) {
         super("User " + username + " not found");
