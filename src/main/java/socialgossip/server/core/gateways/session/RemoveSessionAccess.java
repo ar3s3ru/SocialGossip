f0756337a1840833e205fcd2a84b19ec824893b4
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-26 10:55:39 +0200

    added RemoveSessionAccess dao

--- src/main/java/socialgossip/server/core/gateways/session/RemoveSessionAccess.java
+++ src/main/java/socialgossip/server/core/gateways/session/RemoveSessionAccess.java
@@ -1,4 +1,8 @@
 package socialgossip.server.core.gateways.session;
 
+import socialgossip.server.core.entities.session.Session;
+import socialgossip.server.core.gateways.GatewayException;
+
 public interface RemoveSessionAccess {
+    void remove(Session session) throws SessionNotFoundException, GatewayException;
 }
