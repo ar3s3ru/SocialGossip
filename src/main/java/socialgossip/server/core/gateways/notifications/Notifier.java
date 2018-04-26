fb2fa68446515b6496cc582260b4cb2e22949270
Danilo Cianfrone <danilocianfr@gmail.com>
2018-04-26 10:55:05 +0200

    added NotificationHandler unregistration from Notifier

--- src/main/java/socialgossip/server/core/gateways/notifications/Notifier.java
+++ src/main/java/socialgossip/server/core/gateways/notifications/Notifier.java
@@ -1,6 +1,9 @@
 package socialgossip.server.core.gateways.notifications;
 
+import socialgossip.server.core.entities.session.SessionScoped;
+
 public interface Notifier {
     void register(NotificationHandler handler);
+    void unregister(SessionScoped scope);
     void send(Notification notification) throws UnsupportedNotificationException;
 }
