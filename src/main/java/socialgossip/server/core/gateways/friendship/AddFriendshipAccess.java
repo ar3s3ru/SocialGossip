a6e2f61841c72f61f041f4ddcc8913f1159649e1
Danilo Cianfrone <danilocianfr@gmail.com>
2018-05-13 17:53:16 +0200

    added Friendship access

--- src/main/java/socialgossip/server/core/gateways/friendship/AddFriendshipAccess.java
+++ src/main/java/socialgossip/server/core/gateways/friendship/AddFriendshipAccess.java
@@ -1,4 +1,8 @@
 package socialgossip.server.core.gateways.friendship;
 
-public class AddFriendshipAccess {
+import socialgossip.server.core.entities.friendship.Friendship;
+import socialgossip.server.core.gateways.GatewayException;
+
+public interface AddFriendshipAccess {
+    void add(Friendship friendship) throws FriendshipAlreadyExistsException, GatewayException;
 }
--- src/main/java/socialgossip/server/core/gateways/friendship/FriendshipAlreadyExistsException.java
+++ src/main/java/socialgossip/server/core/gateways/friendship/FriendshipAlreadyExistsException.java
@@ -1,4 +1,12 @@
 package socialgossip.server.core.gateways.friendship;
 
-public class FriendshipAlreadyExistsException {
+import socialgossip.server.core.entities.friendship.Friendship;
+
+public class FriendshipAlreadyExistsException extends Exception {
+    public FriendshipAlreadyExistsException(final Friendship friendship) {
+        super("friendship between " +
+                friendship.getSubjects()[0].getId() +
+                " and " +
+                friendship.getSubjects()[1].getId() + " already exists");
+    }
 }
