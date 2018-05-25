420339d8ce6abf0e86a88446777185c208d35b75
Danilo Cianfrone <danilocianfr@gmail.com>
2018-05-25 03:42:15 +0200

    added Registration presenter

--- src/main/java/socialgossip/server/presentation/Presenter.java
+++ src/main/java/socialgossip/server/presentation/Presenter.java
@@ -1,4 +1,25 @@
 package socialgossip.server.presentation;
 
-public interface Presenter {
+import org.json.simple.JSONObject;
+
+public interface Presenter<ResultType> {
+    default JSONObject getOkResponse(ResultType result) {
+        final JSONObject response = new JSONObject();
+        response.put("result", "success");
+        return response;
+    }
+
+    default JSONObject getFailResponse(Throwable fail) {
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("response", "fail");
+        jsonObject.put("message", fail.getMessage());
+        return jsonObject;
+    }
+
+    default JSONObject getErrorResponse(Throwable error) {
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("response", "error");
+        jsonObject.put("message", error.getMessage());
+        return jsonObject;
+    }
 }
--- src/main/java/socialgossip/server/presentation/registration/RegistrationPresenter.java
+++ src/main/java/socialgossip/server/presentation/registration/RegistrationPresenter.java
@@ -1,4 +1,13 @@
 package socialgossip.server.presentation.registration;
 
-public class RegistrationPresenter {
+import org.json.simple.JSONObject;
+import socialgossip.server.presentation.Presenter;
+
+public final class RegistrationPresenter implements Presenter<String> {
+    @Override
+    public JSONObject getOkResponse(final String result) {
+        final JSONObject jsonObject = Presenter.super.getOkResponse(result);
+        jsonObject.put("username", result);
+        return jsonObject;
+    }
 }
